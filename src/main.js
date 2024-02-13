4300829f41c068017d89dfc731d7651387175ff8
Артемий1~ <senior423it@gmail.com>
2024-02-13 13:50:49 +0200

    feat(main.js): module theme

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { buttonAdd, modal, taskListTodo, taskListProgress, taskListDone } from "./components.js";
+import { buttonAdd, modal, taskListTodo, taskListProgress, taskListDone } from "./components.js"
 import { columnList, counterTodo, counterProgress, counterDone, deleteAllButton } from "./components.js"
 import { editIcon, deleteIcon, moveIcon, cancelIcon, completeIcon } from "./components.js"
 
@@ -6,68 +6,7 @@ import { getTasks, setTasks } from "./localStorage.js"
 
 import "./clock.js"
 
-// MODULE__THEME 
-
-//обработчик событий
-const toggleThemeButton = document.getElementById('toggle_theme')
-toggleThemeButton.addEventListener('click', toggleTheme)
-
-const getTheme = (theme) => {
-  return JSON.parse(localStorage.getItem(theme) ?? '[]')
- }
- 
- const setTheme = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value))
- }
-
-//функция переключения темы
-   
-  function toggleTheme(){
-    if(getTheme('theme') === 'dark'){
-    document.documentElement.setAttribute('theme', 'light');  
-    setTheme('theme', 'light')
-    styleLightTheme()
-
-  }
-  else{
-    document.documentElement.setAttribute('theme', 'dark');
-    setTheme('theme', 'dark')
-    styleDarkTheme()
-
-  }
-
-}
-
-function styleLightTheme (){
-
-  document.body.style.backgroundSize = 'auto';
-  document.body.style.webkitAnimation = 'none';
-  document.body.style.mozAnimation = 'none';
-  document.body.style.oAnimation = 'none';
-  document.body.style.animation = 'none';
-}
-
-function styleDarkTheme (){
-  document.body.style.background = 'linear-gradient(2000deg, rgb(32 32 37), rgb(24 21 21), rgb(55 56 57), rgb(29 28 30)))';
-  document.body.style.backgroundSize = '100000% 100000%';
-  document.body.style.webkitAnimation = 'ServiceAnimation 10s ease infinite';
-  document.body.style.mozAnimation = 'ServiceAnimation 10s ease infinite';
-  document.body.style.oAnimation = 'ServiceAnimation 10s ease infinite';
-  document.body.style.animation = 'ServiceAnimation 10s ease infinite';
-
-}
-
-//toggle theme
-
-const init = () =>{
-  let theme = null
-  getTheme('theme').length > 0 ? theme = getTheme('theme') : theme = 'light'
-  document.documentElement.setAttribute('theme', `${theme}`)
-}
-init()
-
-
-
+import "./theme.js"
 
 function removeAllItems() {
   const tasks = getTasks()
